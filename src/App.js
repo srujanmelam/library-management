e3c031da5647f33998a9a92a1d18e1e84a580ffc
manoj-b-22 <manojsrinivas.b19@iiits.in>
2021-12-12 11:31:24 +0530

    added db.json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import About from '../src/Component/About';
 import UserSignIn from './Component/UserSignIn';
@@ -8,28 +7,23 @@ import UserHomePage from './Component/UserHomePage';
 import AdminHomePage from './Component/AdminHomePage';
 import AddBooks from './AddBooks';
 import SearchBooks from './SearchBooks';
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Link
-} from 'react-router-dom';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
-  
+
     <div className="App">
       <Router>
         <NavBar />
-        <Routes>
-          <Route exact path='/SearchBooks' element={< SearchBooks />}></Route>
-          <Route exact path='/AddBooks' element={< AddBooks />}></Route>
-          <Route exact path='/AdminHomePage' element={< AdminHomePage />}></Route>
-          <Route exact path='/UserHomePage' element={< UserHomePage />}></Route>
+        <Switch>
+          <Route exact path='/SearchBooks' component={SearchBooks}/>
+          <Route exact path='/AddBooks' component={AddBooks}/>
+          <Route exact path='/AdminHome' component={AdminHomePage}/>
+          <Route exact path='/Home' element={< UserHomePage />}></Route>
           <Route exact path='/' element={< UserSignIn />}></Route>
           <Route exact path='/about' element={< About />}></Route>
           <Route exact path='/contact' element={< Contact />}></Route>
-        </Routes>  
+        </Switch>  
       </Router>
     </div>
   );
